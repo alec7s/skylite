fd15d8a9bc3b4a843bef5c6f451dc12733244e47
alec7s <alex.c.evans1@gmail.com>
2023-07-09 19:07:13 -0400

    revised netlify function

--- src/data/weather-api.js
+++ src/data/weather-api.js
@@ -4,9 +4,8 @@ export const WeatherAPI = (function(){
 
     async function _fetchForecastData(input) {
         try {
-            const netlifyResponse = '/.netlify/ext-api/key'; 
-            const key = netlifyResponse.message;
-            console.log(netlifyResponse)
+            //const netlifyResponse = '/.netlify/ext-api/key'; 
+            const key = '/.netlify/ext-api/key';
             console.log(key);
             return await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${input}&APPID=${key}&units=imperial`, {mode: 'cors'});
         }
